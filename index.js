7335bd35f0b0ec8763b079008ea91a455365b6b8
adarsh500 <73278151+adarsh500@users.noreply.github.com>
2021-09-04 19:55:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const httpServer = http.createServer(app);
 
 const io = socketio(httpServer, {
   cors: {
-    origin: "https://fireechat.netlify.app/",
+    origin: "https://fireechat.netlify.app",
   },
 });
 
